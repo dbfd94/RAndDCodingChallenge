97c5783ba9036cdeaaf5ee159ba269b974943f4d
Daniel B. Fortin <Daniel.Fortin@cwc.com>
2020-02-24 12:28:55 -0600

    Add project files.

---
+++ ConsoleApp8/People.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp8
+{
+    class People
+    {
+        private int PersonID { get; set; }
+        private string Name { get; set; }
+        private string LastName { get; set; }
+        private string CurrentRole { get; set; }
+        private string Country { get; set; }
+        private string Industry { get; set; }
+        private int NumberOfRecommendations { get; set; }
+        private int NumberOfConnections { get; set; }
+        public People(int personID, string name, string lastName, string currentRole, string country, string industry,
+            int numberOfRecommendations, int numberOfConnections)
+        {
+            this.PersonID = personID;
+            this.Name = name;
+            this.LastName = lastName;
+            this.CurrentRole = currentRole;
+            this.Country = country;
+            this.Industry = industry;
+            this.NumberOfRecommendations = numberOfRecommendations;
+            this.NumberOfConnections = numberOfConnections;
+        }
+        public override string ToString()
+        {
+            return $"PersonID: {PersonID} \nName: {Name} \nLast Name: {LastName} \nCurrent Role: {CurrentRole} \nCountry: {Country} " +
+                $"\nIndustry: {Industry} \nNumber of Recommendations:{NumberOfRecommendations} " +
+                $"\nNumber of Connections:{NumberOfConnections}";
+        }
+
+    }
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the path + the file name:");
+            string filePath = Console.ReadLine();
+
+            string[] userInfo = File.ReadAllLines(filePath);
+            int amountOfUsersInFile = userInfo.Length;
+            Console.WriteLine($"The file has a total of {amountOfUsersInFile} users.");
+
+            var persons = new List<People>(amountOfUsersInFile);
+
+            int[] personIDs = new int[amountOfUsersInFile];
+            string[] names = new string[amountOfUsersInFile];
+            string[] lastNames = new string[amountOfUsersInFile];
+            string[] currentRoles = new string[amountOfUsersInFile];
+            string[] countrys = new string[amountOfUsersInFile];
+            string[] industries = new string[amountOfUsersInFile];
+            int[] numberOfRecommendationss = new int[amountOfUsersInFile];
+            int[] numberOfConnectionss = new int[amountOfUsersInFile];
+
+            int counter = 0;
+            int pipeLocation;
+            string infoModified;
+            foreach (string info in userInfo)
+            {
+
+                pipeLocation = info.IndexOf('|');
+                personIDs[counter] = int.Parse(info.Substring(0, pipeLocation));
+                infoModified = info.Substring(pipeLocation + 1);
+                pipeLocation = infoModified.IndexOf('|');
+                names[counter] = infoModified.Substring(0, pipeLocation);
+                infoModified = infoModified.Substring(pipeLocation + 1);
+                pipeLocation = infoModified.IndexOf('|');
+                lastNames[counter] = infoModified.Substring(0, pipeLocation);
+                infoModified = infoModified.Substring(pipeLocation + 1);
+                pipeLocation = infoModified.IndexOf('|');
+                currentRoles[counter] = infoModified.Substring(0, pipeLocation);
+                infoModified = infoModified.Substring(pipeLocation + 1);
+                pipeLocation = infoModified.IndexOf('|');
+                countrys[counter] = infoModified.Substring(0, pipeLocation);
+                infoModified = infoModified.Substring(pipeLocation + 1);
+                pipeLocation = infoModified.IndexOf('|');
+                industries[counter] = infoModified.Substring(0, pipeLocation);
+                infoModified = infoModified.Substring(pipeLocation + 1);
+                pipeLocation = infoModified.IndexOf('|');
+                numberOfRecommendationss[counter] = int.Parse(info.Substring(0, pipeLocation));
+                infoModified = infoModified.Substring(pipeLocation + 1);
+                pipeLocation = infoModified.IndexOf('|');
+                numberOfConnectionss[counter] = int.Parse(infoModified);
+                counter++;
+            }
+            for(int i = 0; i < amountOfUsersInFile; i++)
+            {
+                persons.Add(new People(personIDs[i], names[i], lastNames[i], currentRoles[i], countrys[i], industries[i],
+                    numberOfRecommendationss[i], numberOfConnectionss[i]));
+            }
+
+            Console.WriteLine("The industries people in the file work at are the following:");
+            string industriesConcat = "";
+            int numberOfIndustries = 0;
+
+            foreach (string industry in industries)
+            {
+                if (industriesConcat.IndexOf(industry) < 0)
+                {
+                    industriesConcat += industry;
+                    Console.WriteLine(industry);
+                    numberOfIndustries++;
+                }
+            }
+            Console.WriteLine($"There are a total of {numberOfIndustries} industries.");
+            Console.WriteLine(persons[1906]);
+
+
+
+
+
+        }
+    }
+}
